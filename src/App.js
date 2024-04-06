b0292ccc02c0d72088975964d5f64425809d1fb6
Jasim Uddin <jasimbdpro@gmail.com>
2024-04-06 20:04:49 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [nayok, setNayok] = useState([])
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setNayok(data))
-      .catch(error => console.error('Data Failed to Load'))
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <Nayoks></Nayoks>
-        <ul>
-          {nayok.map(nk => <li>{nk.name}</li>)}
-        </ul>
-      </header>
+    <div>
+     
     </div>
   );
 }
-
-function Nayoks(props) {
-  return (
-    <div>
-      <h1> Ami nayok: {props.nameki} </h1>
-      <p>tui amake chinis?</p>
-
-    </div>
-  )
-}
-
 export default App;
