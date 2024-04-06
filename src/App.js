32512da7fc44bf43f8b22583032628fe633008b8
Jasim Uddin <jasimbdpro@gmail.com>
2024-04-06 11:39:21 +0600

    nw

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Nayoks></Nayoks>
+        <Nayoks></Nayokssssssssssss>
         <ul>
           {nayok.map(nk => <li>{nk.name}</li>)}
         </ul>
