a4a2a67c1e97a66bdee9c5a8dd17c09bc17d788a
Jasim Uddin <jasimbdpro@gmail.com>
2024-04-16 09:01:41 +0600

    new

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Shop.css';
+import products from '../../fakeData/products.json'
 
 const Shop = () => {
+    const [productn, setProductn] = useState(products.slice(0, 11));
     return (
         <div>
             <h1>This is Shop</h1>
+            <h3>
+                {productn.length}
+            </h3>
+            <ul>
+                {productn.map(product => (
+                    <li key={product.id}>{product.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
