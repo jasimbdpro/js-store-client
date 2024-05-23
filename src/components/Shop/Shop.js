697236d06c35483be70b18249a2c16827bc802f3
Jasim Uddin <jasimbdpro@gmail.com>
2024-05-23 09:14:57 +0600

    nw

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import productData from '../../fakeData/products.json'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const products = productData.slice(0, 15);
@@ -13,17 +13,31 @@ const Shop = () => {
         setCart(newCart);
         addToDb(product.id)
     }
+    useEffect(() => {
+        //cart
+        const savedCart = getShoppingCart()
+        const productIds = Object.keys(savedCart);
+        const cartProducts = productIds.map(id => {
+            const product = productData.find(i => i.id === id)
+            product.quantity = savedCart[id]
+            return product;
+        });
+        setCart(cartProducts);
+
+    }, [])
     return (
         <div className='shop-container'>
             <div className="product-container">
 
-                {products.map(i =>
-                    <Product
+                {products.map(i => {
+
+                    return <Product
                         showAddToCart={true}
                         handleAddProduct={handleAddProduct}
                         product={i}
                         key={i.id}
                     ></Product>
+                }
                 )}
 
             </div>
